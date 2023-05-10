240efc3ec1fc7cdac3f8b5bf74fe2c4f49866368
Ashley Cooper <122837620+ashley-mspgeek@users.noreply.github.com>
2023-05-10 15:48:07 -0400

    testing adding link to forwarded message

--- bridge/discord/discord.go
+++ bridge/discord/discord.go
@@ -279,6 +279,21 @@ func (b *Bdiscord) Send(msg config.Message) (string, error) {
 	if msg.ParentNotFound() {
 		msg.ParentID = ""
 	}
+    // check if "slack_attachment" key exists in the Extra field
+	var fromURL string
+	if extra, ok := msg.Extra["slack_attachment"]; ok {
+		for _, attachment := range extra {
+			if a, ok := attachment.([]slack.Attachment); ok {
+				for _, attach := range a {
+					if attach.FromURL != "" {
+						fromURL = attach.FromURL
+						break
+					}
+				}
+			}
+		}
+	}
+	msg.Text = fromURL + "\n" + msg.Text
 
 	// Use webhook to send the message
 	useWebhooks := b.shouldMessageUseWebhooks(&msg)
